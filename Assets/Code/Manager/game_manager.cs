a8bff7e37d071e894e0fbc99ea174abecd7264ac
EHChan52 <126001690+EHChan52@users.noreply.github.com>
2023-03-22 19:11:26 +0800

    Update game_manager.cs

--- Assets/Code/Manager/game_manager.cs
+++ Assets/Code/Manager/game_manager.cs
@@ -10,10 +10,19 @@ public class game_manager : MonoBehaviour{
     public int frameRate;
 
 
-    private int lives,scores;
+    private int lives=3,scores=0;
+    private int energizer_number,energizer_number_least;
+    private int pellet_number,pellet_eaten,pellet_score=5,ghost_score=100;
+    
     void Awake(){
         frameRate=25;
         Application.targetFrameRate=frameRate;
+    }
+    void Start(){
+    pellet_eaten=scores=0;
+    }
+    void Update(){
+    
     }
 
 }
